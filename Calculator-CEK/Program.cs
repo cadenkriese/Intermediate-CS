094fad2e519f906c621be12efcc3bbae882669b1
Caden <caden2k3@gmail.com>
2019-01-10 13:15:40 -0700

    Update calculator

--- Calculator-CEK/Program.cs
+++ Calculator-CEK/Program.cs
@@ -1,99 +1,115 @@
 ﻿using System;
-using System.Text.RegularExpressions;
 
 namespace Calculator_CEK
 {
-    internal static class Program
+    internal static class CalculatorCek
     {
+        private static Boolean _goNext = true;
+
         private static void Main()
         {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-
-            Console.WriteLine(
-                "  _________  ____  _________  / /__     _________ _/ /______  __/ /___ _/ /_____  _____");
-            Console.WriteLine(
-                " / ___/ __ \\/ __ \\/ ___/ __ \\/ / _ \\   / ___/ __ `/ / ___/ / / / / __ `/ __/ __ \\/ ___/");
-            Console.WriteLine(
-                "/ /__/ /_/ / / / (__  ) /_/ / /  __/  / /__/ /_/ / / /__/ /_/ / / /_/ / /_/ /_/ / /    ");
-            Console.WriteLine(
-                "\\___/\\____/_/ /_/____/\\____/_/\\___/   \\___/\\__,_/_/\\___/\\__,_/_/\\__,_/\\__/\\____/_/     ");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Welcome to ConsoleCalculator V1.0");
-            Console.WriteLine("Valid operators, +, -, *, /, %, abs, sin, cos, tan, end");
-
-            //Ask for operation first so we know the amount of inputs required.
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Please enter an operation.");
-            var operation = Console.In.ReadLine();
-
-            if (operation.ToLower().Equals("end"))
+            while (_goNext)
             {
-                Console.ForegroundColor = ConsoleColor.White;
-                return;
-            }
+                Console.ForegroundColor = ConsoleColor.Cyan;
 
-            //Perform calculations that only require one number first.
-            if (operation.ToLower().Equals("sin") || operation.ToLower().Equals("cos") ||
-                operation.ToLower().Equals("tan") || operation.ToLower().Equals("abs") ||
-                operation.ToLower().Equals("end"))
-            {
-                var input = RetrieveNumber("Enter a number.");
-                operation = operation.ToLower();
+                for (var i = 0; i < 6; i++)
+                {
+                    Console.WriteLine("");
+                }
+
+                Console.WriteLine(
+                    "  _________  ____  _________  / /__     _________ _/ /______  __/ /___ _/ /_____  _____");
+                Console.WriteLine(
+                    " / ___/ __ \\/ __ \\/ ___/ __ \\/ / _ \\   / ___/ __ `/ / ___/ / / / / __ `/ __/ __ \\/ ___/");
+                Console.WriteLine(
+                    "/ /__/ /_/ / / / (__  ) /_/ / /  __/  / /__/ /_/ / / /__/ /_/ / / /_/ / /_/ /_/ / /    ");
+                Console.WriteLine(
+                    "\\___/\\____/_/ /_/____/\\____/_/\\___/   \\___/\\__,_/_/\\___/\\__,_/_/\\__,_/\\__/\\____/_/     ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Welcome to ConsoleCalculator V1.0");
+                Console.WriteLine("Valid operators, +, -, *, /, %, abs, sin, cos, tan, end");
+
+                //Ask for operation first so we know the amount of inputs required.
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Please enter an operation.");
+                var operation = Console.In.ReadLine();
+
+                //Perform calculations that only require one number first.
+                if (operation.ToLower().Equals("sin") || operation.ToLower().Equals("cos") ||
+                    operation.ToLower().Equals("tan") || operation.ToLower().Equals("abs"))
+                {
+                    var input = RetrieveNumber("Enter a number.");
+                    operation = operation.ToLower();
+
+                    switch (operation)
+                    {
+                        case "sin":
+                            Console.WriteLine("sin(" + input + ") = " + ConvertRadiansToDegrees(Math.Sin(input)) + "°");
+                            break;
+                        case "cos":
+                            Console.WriteLine("cos(" + input + ") = " + ConvertRadiansToDegrees(Math.Cos(input)) + "°");
+                            break;
+                        case "tan":
+                            Console.WriteLine("tan(" + input + ") = " + ConvertRadiansToDegrees(Math.Tan(input)) + "°");
+                            break;
+                        case "abs":
+                            Console.WriteLine("abs(" + input + ") = " + ConvertRadiansToDegrees(Math.Abs(input)) + "°");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid argument, type END to exit.");
+                            break;
+                    }
+
+                    _goNext = GoAgane();
+                    continue;
+                }
+
+                //Run operations that require 2 numbers.
+                var firstNumber = RetrieveNumber("Please enter a number.");
+                var secondNumber = RetrieveNumber("Please enter a second number.");
+
+                if (firstNumber == double.MinValue || secondNumber == double.MinValue)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid number!");
+                    _goNext = GoAgane();
+                    continue;
+                }
 
                 switch (operation)
                 {
-                    case "sin":
-                        Console.WriteLine("sin(" + input + ") = " + Math.Sin(input));
+                    case "+":
+                        Console.WriteLine(firstNumber + " + " + secondNumber + " = " + (firstNumber + secondNumber));
                         break;
-                    case "cos":
-                        Console.WriteLine("cos(" + input + ") = " + Math.Cos(input));
+                    case "-":
+                        Console.WriteLine(firstNumber + " - " + secondNumber + " = " + (firstNumber - secondNumber));
                         break;
-                    case "tan":
-                        Console.WriteLine("tan(" + input + ") = " + Math.Tan(input));
+                    case "*":
+                        Console.WriteLine(firstNumber + " * " + secondNumber + " = " + (firstNumber * secondNumber));
                         break;
-                    case "abs":
-                        Console.WriteLine("abs(" + input + ") = " + Math.Abs(input));
+                    case "/":
+                        if (secondNumber == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Cannot divide by 0!");
+                            _goNext = GoAgane();
+                            continue;
+                        }
+
+                        String remainder = (firstNumber % secondNumber != 0 && ((int)firstNumber / (int)secondNumber != 0)) ? " with a remainder of " + firstNumber % secondNumber : "";
+                        Console.WriteLine(firstNumber + " / " + secondNumber + " = " + ((int)firstNumber / (int)secondNumber) + remainder);
+                        break;
+                    case "%":
+                        Console.WriteLine(firstNumber + " % " + secondNumber + " = " + firstNumber % secondNumber);
                         break;
                     default:
-                        Console.WriteLine("Invalid argument, type END to exit.");
+                        Console.WriteLine("Invalid operation!");
                         break;
                 }
 
-                Console.ForegroundColor = ConsoleColor.White;
-                return;
+                _goNext = GoAgane();
+                continue;
             }
-
-            //Run operations that require 2 numbers.
-            var firstNumber = RetrieveNumber("Please enter a number.");
-            var secondNumber = RetrieveNumber("Please enter a second number.");
-
-            //Remove any alphanumeric characters to allow users to mistype and the program still works.
-            var rgx = new Regex("[a-zA-Z0-9 ]");
-            operation = rgx.Replace(operation, "");
-
-            switch (operation)
-            {
-                case "+":
-                    Console.WriteLine(firstNumber + " + " + secondNumber + " = " + (firstNumber + secondNumber));
-                    break;
-                case "-":
-                    Console.WriteLine(firstNumber + " - " + secondNumber + " = " + (firstNumber - secondNumber));
-                    break;
-                case "*":
-                    Console.WriteLine(firstNumber + " * " + secondNumber + " = " + firstNumber * secondNumber);
-                    break;
-                case "/":
-                    Console.WriteLine(firstNumber + " / " + secondNumber + " = " + firstNumber / secondNumber);
-                    break;
-                case "%":
-                    Console.WriteLine(firstNumber + " % " + secondNumber + " = " + firstNumber % secondNumber);
-                    break;
-                default:
-                    Console.WriteLine("Invalid operation!");
-                    break;
-            }
-
-            Console.ForegroundColor = ConsoleColor.White;
         }
 
         /// <summary>
@@ -108,6 +124,23 @@ namespace Calculator_CEK
             Console.WriteLine(request);
             var numberInput = Console.In.ReadLine();
             return double.TryParse(numberInput, out var doubleInput) ? doubleInput : double.MinValue;
+        }
+
+        private static Boolean GoAgane()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Would you like to go again?");
+            String input = Console.In.ReadLine();
+
+            //If they say no the console color has to be set to white.
+            Console.ForegroundColor = ConsoleColor.White;
+            return input.Equals("yes", StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private static double ConvertRadiansToDegrees(double radians)
+        {
+            double degrees = (180 / Math.PI) * radians;
+            return (degrees);
         }
     }
 }
