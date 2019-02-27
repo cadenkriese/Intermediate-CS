d7164a74f7df35c77ce3336b97da56aadf2dba0b
Caden <caden2k3@gmail.com>
2019-02-27 12:16:39 -0700

    Create SurfaceArea-CEK

---
+++ SurfaceArea-CEK/SurfaceArea.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using RectPrisms;
+
+namespace SurfaceArea_CEK
+{
+    class SurfaceArea
+    {
+        static void Main(string[] args)
+        {
+            RectangularPrism prism = new RectangularPrism();
+            prism.Input();
+            prism.CalculateVolume();
+            prism.OutputVolume();
+            prism.CalculateSurfaceArea();
+            prism.OutputSurfaceArea();
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+
+    
+    class RectangularPrism : RectPrism
+    {
+        private int SurfaceArea;
+
+        public void CalculateSurfaceArea()
+        {
+            SurfaceArea = 2 * (width * length + height * length + height * width);
+        }
+        
+        public void OutputSurfaceArea()
+        {
+            Console.Out.WriteLine("The surface area of the rectangular prism "+SurfaceArea+" square units.");
+        }
+    }
+}
