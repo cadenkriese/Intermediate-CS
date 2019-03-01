9f2b6e6e76d0ae3d3148dcd18116347feb48fcd6
Caden <caden2k3@gmail.com>
2019-03-01 13:14:24 -0700

    Update CheckWriter-CEK

--- CheckWriter-CEK/CheckWriter_CEK.cs
+++ CheckWriter-CEK/CheckWriter_CEK.cs
@@ -7,21 +7,26 @@ namespace CheckWriter_CEK
     {
         static void Main(string[] args)
         {
-            CheckUpdated check = new CheckUpdated();
-            check.Display();
+            int width = 65;
+            
+            String date = DateTime.Today.ToString("MMMM dd, yyyy");
+            Console.Out.WriteLine(new string(' ', width-date.Length)+date);
         }
     }
 
     class CheckUpdated : Check
-    {
-        private String date;
-        
+    {   
         public override void Display()
         {
             int width = 65;
             
-            date = DateTime.Today.ToString("MMMM dd, yyyy");
+            string date = DateTime.Today.ToString("MMMM dd, yyyy");
+            string amountString = "$ " + Amount;
+                
             Console.Out.WriteLine(new string(' ', width-date.Length)+date);
+            Console.Out.WriteLine("Pay to the ");
+            Console.Out.WriteLine("Order of: "+Payee);
+            Console.Out.WriteLine(new String(' ', width-amountString.Length)+amountString);
         }
     }
 }
