c54509553b0efcb507b0c55d0f29fcf0cfd1fd91
Caden <caden2k3@gmail.com>
2019-03-01 12:55:57 -0700

    Create CheckWriter-CEK

---
+++ CheckWriter-CEK/CheckWriter_CEK.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CheckWriter;
+
+namespace CheckWriter_CEK
+{
+    class CheckWriter_CEK
+    {
+        static void Main(string[] args)
+        {
+            CheckUpdated check = new CheckUpdated();
+            check.Display();
+        }
+    }
+
+    class CheckUpdated : Check
+    {
+        private String date;
+        
+        public override void Display()
+        {
+            int width = 65;
+            
+            date = DateTime.Today.ToString("MMMM dd, yyyy");
+            Console.Out.WriteLine(new string(' ', width-date.Length)+date);
+        }
+    }
+}
